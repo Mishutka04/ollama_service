c7822841f2a627c912d632adabf58a7dd11d5016
Mishutka04 <vitalykodolov@yandex.ru>
2024-12-01 01:57:10 +0300

    Fix docker

--- swagger/src/ollama_app/dependencies.py
+++ swagger/src/ollama_app/dependencies.py
@@ -4,17 +4,19 @@ from fastapi import HTTPException
 import openai
 
 from ollama_app.schemas import SOllamaModel
-
+from openai import OpenAI
 load_dotenv()
 
 # Set your OpenAI API key and base URL
 openai.api_key = 'ollama'  # Replace with your actual API key
 base_url = 'http://localhost:11434/v1'  # Replace with your actual base URL
-
+client = OpenAI(
+    api_key='ollama',  # This is the default and can be omitted
+)
 async def qween_api(comment: SOllamaModel):
     
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model="qwen2.5:32b",
             messages={"role": comment.role, "content": comment.content},
             api_base=base_url  # Use the specified base URL
